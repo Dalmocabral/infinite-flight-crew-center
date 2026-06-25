5d24f595684278fa3530fc95661edc038d22fa3f
Dalmo Cabral <dalmo.santos.cabral@gmail.com>
2026-06-25 15:28:36 -0300

    fix: translate validation error to English

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -15,7 +15,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def validate(self, data):
         if data['password'] != data['confirm_password']:
-            raise serializers.ValidationError("As senhas não coincidem.")
+            raise serializers.ValidationError("Passwords do not match.")
         return data
 
     def create(self, validated_data):
